a103744759136892af01ef0d5973201ca3a34f33
jona42-ui <jonathanthembo123@gmail.com>
2022-03-18 13:10:23 +0300

    second commit

--- application.py
+++ application.py
@@ -2,12 +2,13 @@ import datetime
 from symtable import SymbolTable
 from xml.dom import NO_MODIFICATION_ALLOWED_ERR
 from flask import Flask, render_template
-
+import time
 app = Flask(__name__)
 
 @app.route("/")
+@app.route("/home")
 def index():
     now = datetime.datetime.now()
-    sabbath_time = now.time==19 and now.day==6
+    sabbath_time = now.time==19 and now.day==7
     return render_template("index.html", sabbath_time=sabbath_time)
 
