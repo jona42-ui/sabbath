6561578c1f03fd110c10201b81e62caa0e432e9b
jona42-ui <jonathanthembo123@gmail.com>
2022-03-22 20:28:13 +0300

    another commit

--- application.py
+++ application.py
@@ -1,14 +1,25 @@
 import datetime
 from symtable import SymbolTable
 from xml.dom import NO_MODIFICATION_ALLOWED_ERR
-from flask import Flask, render_template
-import time
+from flask import Flask, flash, redirect, render_template, request, session, abort
+from random import randint
+import shabbos_web_class
+import googleapi
+
 app = Flask(__name__)
-
+ 
 @app.route("/")
-@app.route("/home")
 def index():
-    now = datetime.datetime.now()
-    sabbath_time = now.time==19 and now.day==7
-    return render_template("index.html", sabbath_time=sabbath_time)
+    #return name
+    
 
+    Candletime = shabbos_web_class.return_candletime_string()
+    countdown = shabbos_web_class.time_remaining()
+   
+
+
+    return render_template(
+        'index.html',**locals())
+ 
+if __name__ == "__main__":
+    app.run()
---
+++ googleapi.py
@@ -0,0 +1,39 @@
+from grabIP import IPadd
+key1 = "AIzaSyAMrFVsqH7M0lB-1K9Sf1x3E8yoN3VFIwg"
+
+
+# def IPadd():
+# 	from flask import request
+# 	try:
+# 		local_IP = request.environ['HTTP_X_FORWARDED_FOR']
+# 		return local_IP
+# 	except:
+# 		print("error in IPadd")
+# 		# generic IP from internet society of israel in jerusalem
+# 		return "74.105.72.198"
+
+def local_time():
+	import requests, json, datetime, time
+	#grabs your latitude, longitude and Time zone
+	IPdata = IPadd()
+	loc_request=requests.get('http://ip-api.com/json/' + IPdata)
+	type(loc_request)
+	loc_request.status_code==requests.codes.ok
+	loc_request_json_data = loc_request.text
+	Location_info = json.loads(loc_request_json_data)
+	longi = str(Location_info['lon'])
+	latit = str(Location_info['lat'])
+	key1 = "AIzaSyAMrFVsqH7M0lB-1K9Sf1x3E8yoN3VFIwg"
+	fromepoch = str(int(time.time()))
+	loc_request=requests.get("https://maps.googleapis.com/maps/api/timezone/json?location=" + latit + ","+ longi +"&timestamp=" + fromepoch+"&key=" + key1)
+	type(loc_request)
+	loc_request.status_code==requests.codes.ok
+	loc_request_json_data = loc_request.text
+	Location_info = json.loads(loc_request_json_data)
+	dtos = Location_info['dstOffset']
+	raw = Location_info['rawOffset']
+
+	longtime = float(dtos) + float(raw) + float(fromepoch)
+
+	date_time_obj = datetime.datetime.fromtimestamp(longtime)
+	return date_time_obj
